e1c4da8e8a34306fbe9e49b7864382d06bc71e0f
João Pelicer <jpelicer12@yahoo.com>
2026-03-02 21:51:18 -0300

    chore: configure docker-compose and prisma database

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import "dotenv/config";
 
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 import Fastify from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
@@ -15,10 +18,27 @@ const app = Fastify({
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-// // Declare a route
-// app.get("/", async function handler() {
-//   return { hello: "world" };
-// });
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Treinos API",
+      description: "API for the Treinos project",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "LocalHost",
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+//rota da documentação
+await app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
 
 app.withTypeProvider<ZodTypeProvider>().route({
   method: "GET",
