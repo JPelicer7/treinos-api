6be4ea14ce78025208893711d6a2d79f6fe81d9c
João Pelicer <jpelicer12@yahoo.com>
2026-03-03 19:04:38 -0300

    feat: add workout session table and cascade deletion to schema

---
+++ src/schemas/index.ts
@@ -0,0 +1,32 @@
+import z from "zod";
+
+import { weekDay } from "../generated/prisma/enums.js";
+
+export const ErrorSchema = z.object({
+  error: z.string(),
+  code: z.string(),
+});
+
+export const WorkoutPlanSchema = z.object({
+  id: z.uuid(),
+  name: z.string().trim().min(1, {
+    message: "Name is required",
+  }),
+  workoutDays: z.array(
+    z.object({
+      name: z.string().trim().min(1), //superiores/ inferiores/ etc
+      weekDay: z.enum(weekDay),
+      isRest: z.boolean().default(false),
+      estimatedDurationInSeconds: z.number().min(1),
+      exercises: z.array(
+        z.object({
+          order: z.number().min(0),
+          name: z.string().trim().min(1),
+          sets: z.number().min(1),
+          reps: z.number().min(1),
+          restTimeInSeconds: z.number().min(1),
+        }),
+      ),
+    }),
+  ),
+});
--- src/usecases/CreateWorkoutPlan.ts
+++ src/usecases/CreateWorkoutPlan.ts
@@ -47,6 +47,7 @@ export class CreateWorkoutPlan {
 
       const workoutPlan = await tx.workoutPlan.create({
         data: {
+          id: crypto.randomUUID(),
           name: dto.name,
           userId: dto.userId,
           isActive: true,
